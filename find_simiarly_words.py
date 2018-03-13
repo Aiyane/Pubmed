13ae0e03fc2aa450b563d74861baa1f3cfb6d9d3
aiyane <2310091880@qq.com>
2018-03-13 17:14:00 +0800

    结果问题

--- find_simiarly_words.py
+++ find_simiarly_words.py
@@ -2,21 +2,22 @@
 # -*- coding: utf-8 -*-
 # find_simiarly_words.py
 from gensim.models import word2vec
-import logging, os
+import logging
+import os
 
-logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+logging.basicConfig(
+    format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
-path = '/home/aiyaen/桌面/result_word_embedding'
+path = '/home/aiyane/dir/result_word_embedding'
 
-if not os.path.isdir(path):
-    os.mkdir(path)
 
-trait_path = ''
+trait_path = '/home/aiyane/dir/table/trait_result.txt'
 with open(trait_path, 'r') as fin:
     trait_list = fin.read().split('\n')
 
 for i in range(300, 1000, 200):
-    model = word2vec.Word2Vec.load("/home/aiyane/桌面/model/" + str(i) + "_size.model")
+    model = word2vec.Word2Vec.load(
+        "/home/aiyane/dir/model/" + str(i) + "_size.model")
     with open(path + "/" + str(i) + "_size_result.txt", "w") as f:
         for trait in trait_list:
             try:
